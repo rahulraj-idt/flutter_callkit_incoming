8350630ae0f9092e89b04818176bb97c5b24264e
Rahul <rahul.raj@idt.net>
2025-02-27 13:34:34 +0530

    Fix the incorrect call ending

--- ios/Classes/SwiftFlutterCallkitIncomingPlugin.swift
+++ ios/Classes/SwiftFlutterCallkitIncomingPlugin.swift
@@ -578,9 +578,9 @@ public class SwiftFlutterCallkitIncomingPlugin: NSObject, FlutterPlugin, CXProvi
     public func provider(_ provider: CXProvider, perform action: CXEndCallAction) {
         guard let call = self.callManager.callWithUUID(uuid: action.callUUID) else {
             if(self.answerCall == nil && self.outgoingCall == nil){
-                sendEvent(SwiftFlutterCallkitIncomingPlugin.ACTION_CALL_TIMEOUT, self.data?.toJSON())
+                sendEvent(SwiftFlutterCallkitIncomingPlugin.ACTION_CALL_TIMEOUT, nil)
             } else {
-                sendEvent(SwiftFlutterCallkitIncomingPlugin.ACTION_CALL_ENDED, self.data?.toJSON())
+                sendEvent(SwiftFlutterCallkitIncomingPlugin.ACTION_CALL_ENDED, nil)
             }
             action.fail()
             return
